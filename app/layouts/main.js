faa16cef18daeff1b1b44bf1fa82a2c3f1ad60cb
David <sanderdj90@gmail.com>
2023-11-04 13:58:59 -0600

    save work

--- app/layouts/main.js
+++ app/layouts/main.js
@@ -4,7 +4,7 @@ import { jsx } from 'hono/jsx'
 import { Link } from '../../htmljs';
 
 const MainLayout = async (props) => {
-  const { results: projects } = await props.context.env.DB.prepare("SELECT * FROM projects").all();
+  const { results: libraries } = await props.context.env.DB.prepare("SELECT * FROM LittleLibraries").all();
   const path = new URL(props.context.req.url).pathname
   return (
     <div>
@@ -12,7 +12,7 @@ const MainLayout = async (props) => {
         <div class="flex items-center justify-between w-full px-4 mx-auto 2xl:px-0 max-w-7xl">
           <div class="relative z-10 flex items-center w-auto leading-10 lg:flex-grow-0 lg:flex-shrink-0 lg:text-left">
             <a href="/" class="inline-flex sm:mr-8 items-center font-sans text-2xl font-extrabold text-left text-black no-underline bg-transparent cursor-pointer group focus:no-underline">
-              Todos
+              Little Libraries Albuquerque
               <span class="flex opacity-90 group-hover:scale-150 group-hover:opacity-100 items-center h-full group-hover:-rotate-6 ease-out duration-500 px-0.5 py-px ml-2 -translate-x-px text-[0.65rem] font-bold leading-none border-[2px] rounded border-black -translate-y-px">DEMO</span>
             </a>
             <nav class="items-center hidden space-x-5 text-sm font-medium lg:flex">
@@ -23,12 +23,12 @@ const MainLayout = async (props) => {
             </nav>
           </div>
           <div class="relative space-x-1 font-medium leading-10 sm:space-x-2 md:flex-grow-0 md:flex-shrink-0 md:text-right lg:flex-grow-0 lg:flex-shrink-0">
-            {/* <button class="inline-flex items-center px-3 sm:px-5 text-sm border-0 rounded-md cursor-pointer h-9 focus:outline-none md:mt-0 text-neutral-900 hover:text-neutral-800 hover:bg-neutral-100">
+            <button class="inline-flex items-center px-3 sm:px-5 text-sm border-0 rounded-md cursor-pointer h-9 focus:outline-none md:mt-0 text-neutral-900 hover:text-neutral-800 hover:bg-neutral-100">
               Login
             </button>
             <button class="inline-flex items-center px-3 sm:px-5 text-sm border-0 rounded-md cursor-pointer h-9 focus:outline-none md:mt-0 bg-neutral-900 hover:bg-neutral-800 hover:text-white text-gray-100">
               Signup
-            </button> */}
+            </button>
           </div>
         </div>
       </header>
@@ -38,13 +38,14 @@ const MainLayout = async (props) => {
         <aside class="fixed block w-56 h-screen max-h-screen pt-20 pb-5 pr-5 overflow-scroll bg-white lg:pt-8 lg:pb-16 z-20 top-[55px]">
           <h2 class="relative px-2 py-1 mb-2 text-sm font-semibold rounded-md">My Projects</h2>
           <div class="relative grid grid-flow-row text-sm mb-7 auto-rows-max">
-            {projects.map((project) => (
-              <Link class={`group flex w-full items-center rounded-md border px-2 py-1.5 ${path.startsWith(`/projects/${project.id}`) ? 'bg-gray-100 border-gray-200/60' : 'border-transparent'}`} to={`/projects/${project.id}/view`}>{project.name}</Link>
+            {libraries.map((lib) => (
+              <Link class={`group flex w-full items-center rounded-md border px-2 py-1.5 ${path.startsWith(`/projects/${lib.id}`) ? 'bg-gray-100 border-gray-200/60' : 'border-transparent'}`} to={`/projects/${lib.id}/view`}>{lib.name}</Link>
             ))}
           </div>
         </aside>
         <div id="content" class="relative flex-shrink-0 w-full h-auto py-5 pr-0 pl-64">
           {props.children}
+          {JSON.stringify(libraries)}
         </div>
       </main>
     </div>
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,50 @@
-DROP TABLE IF EXISTS projects;
-CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY, name TEXT);
-INSERT INTO projects (id, name) VALUES (1, 'Shopping List'), (2, 'Errands');
+-- Drop the tables if they exist
+DROP TABLE IF EXISTS LibraryComments;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS LittleLibraries;
 
-DROP TABLE IF EXISTS todos;
-CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, project_id INTEGER, name TEXT, done INTEGER DEFAULT 0, FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE CASCADE);
-INSERT INTO todos (id, project_id, name, done) VALUES (1, 1, 'Buy milk', 1), (2, 1, 'Buy eggs', 0), (3, 2, 'Pick up laundry', 0), (4, 2, 'Get car washed', 0);
+-- Create the LittleLibraries table with an auto-incrementing primary key
+CREATE TABLE LittleLibraries (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL
+);
+
+-- Create the Users table with an auto-incrementing primary key
+CREATE TABLE Users (
+    user_id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+-- Create the LibraryComments table with an auto-incrementing primary key
+CREATE TABLE LibraryComments (
+    comment_id INTEGER PRIMARY KEY,
+    library_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    comment_text TEXT NOT NULL,
+    FOREIGN KEY (library_id) REFERENCES LittleLibraries(id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+-- Insert dummy data into the LittleLibraries table
+INSERT INTO LittleLibraries (name, description, latitude, longitude) VALUES
+    ('Library A', 'A small library with a variety of books.', 40.7128, -74.0060),
+    ('Library B', 'Located in a park, specializes in children''s books.', 34.0522, -118.2437),
+    ('Library C', 'Community library with a wide selection of genres.', 51.5074, -0.1278);
+
+-- Insert dummy data into the Users table with passwords
+INSERT INTO Users (username, email, password) VALUES
+    ('user1', 'user1@example.com', 'password1'),
+    ('user2', 'user2@example.com', 'password2'),
+    ('user3', 'user3@example.com', 'password3');
+
+-- Insert dummy data into the LibraryComments table
+INSERT INTO LibraryComments (library_id, user_id, comment_text) VALUES
+    (1, 1, 'I love this library! It has a great selection of books.'),
+    (2, 2, 'I often bring my kids here. They enjoy it a lot.'),
+    (3, 3, 'This library is a fantastic resource for our community.');
+
